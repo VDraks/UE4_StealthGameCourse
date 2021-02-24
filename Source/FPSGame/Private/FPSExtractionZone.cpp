a22326ddf50861c2eed98eb69ec0e7783deffd03
VDraks <kytinvasya@gmail.com>
2021-02-24 12:33:14 +0300

    add sound to FPSExtractionZone
    20. Add Extraction Feedback

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -5,6 +5,7 @@
 
 #include "Components/BoxComponent.h"
 #include "Components/DecalComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 #include "FPSCharacter.h"
 #include "FPSGameMode.h"
@@ -34,16 +35,25 @@ AFPSExtractionZone::AFPSExtractionZone()
 void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
-
 	AFPSCharacter* MyPawn = Cast<AFPSCharacter>(OtherActor);
-	if (MyPawn && MyPawn->bIsCarryingObjective)
+	if (MyPawn == nullptr)
+	{
+		return;
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Overlapped with extraction zone!"));
+	
+	if (MyPawn->bIsCarryingObjective)
 	{
 		AFPSGameMode* GM = Cast<AFPSGameMode>(GetWorld()->GetAuthGameMode());
 		if (GM)
 		{
 			GM->CompleteMission(MyPawn);
 		}
+	}
+	else
+	{
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound);
 	}
 }
 
